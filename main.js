fcb673430b202718ab2ce1a0ea0e6f5e7f58362c
Kamihela <108040308+Kamihela@users.noreply.github.com>
2026-03-10 00:12:19 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,103 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line")
+});
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+    origin: "bottom",
+    distance: "50px",
+    duration: 1000,
+};
+
+/* Header Scroll Reveal */
+
+ScrollReveal().reveal(".header_image img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+
+ScrollReveal().reveal(".header_content h1", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".header_content h2", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+ScrollReveal().reveal(".header_content p", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+ScrollReveal().reveal(".header_btn", {
+    ...scrollRevealOption,
+    delay: 2000,
+});
+
+/* About Scroll Reveal */
+
+ScrollReveal().reveal(".about_image img", {
+    ...scrollRevealOption,
+    origin: "left",
+});
+
+ScrollReveal().reveal(".about_content .section_header", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".about_content p", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+
+ScrollReveal().reveal(".about_btn", {
+    ...scrollRevealOption,
+    delay: 2000,
+});
+
+/* Service Scroll Reveal */
+
+ScrollReveal().reveal(".service_card", {
+    duration: 1000,
+    interval: 500,
+});
+
+/* Facility Scroll Reveal */
+
+ScrollReveal().reveal(".facility_content .section_header", {
+    ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".facility_content p", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+/* Mentor Scroll Reveal */
+
+ScrollReveal().reveal(".mentor_card", {
+    ...scrollRevealOption,
+    interval: 500,
+});
+
+/* Contact Scroll Reveal */
+
+ScrollReveal().reveal(".banner_content h2", {
+    ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".banner_content p", {
+    ...scrollRevealOption,
+    delay: 500,
+});
